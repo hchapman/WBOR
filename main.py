e98b7984f813287bd42916d87b0c0a8c51ca172d
Harrison Chapman <hchaps@gmail.com>
2010-09-19 22:24:45 -0400

    Favicon, show charts changes

--- main.py
+++ main.py
@@ -44,8 +44,8 @@ class MainPage(webapp.RequestHandler):
     # album_list = models.getNewAlbums(50)
     start = datetime.datetime.now() - datetime.timedelta(weeks=1)
     end = datetime.datetime.now()
-    song_num = 30
-    album_num = 30
+    song_num = 60
+    album_num = 60
     top_songs, top_albums = models.getTopSongsAndAlbums(start, end, song_num, album_num)
     posts = models.getLastPosts(3)
     events = models.getEventsAfter(datetime.datetime.now() - datetime.timedelta(days=1), 3)
